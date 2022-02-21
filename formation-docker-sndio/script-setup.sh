b338953a75c7467f848af0ee23c78f3b7a98c773
Antoine Brunetti <antoine.brunetti@insee.fr>
2022-02-21 16:51:13 +0100

    change stuff

--- formation-docker-sndio/script-setup.sh
+++ formation-docker-sndio/script-setup.sh
@@ -2,6 +2,10 @@
 apt update
 # install jdk
 apt install default-jdk
+# install psql
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+apt update
+apt install postgresql postgresql-contrib postgresql-client-common
 # install maven
 wget https://www-us.apache.org/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz -P /tmp
 tar xf /tmp/apache-maven-*.tar.gz -C /opt
@@ -13,8 +17,3 @@ export PATH=${M2_HOME}/bin:${PATH} \
 " >> /etc/profile.d/maven.sh
 chmod +x /etc/profile.d/maven.sh
 source /etc/profile.d/maven.sh
-# install psql
-apt-get install wget ca-certificates
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-apt update
-apt install postgresql postgresql-contrib postgresql-client-common
