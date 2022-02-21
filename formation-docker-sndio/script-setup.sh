e73acfb8185342197317512bb4d627852748b719
Antoine Brunetti <antoine.brunetti@insee.fr>
2022-02-21 21:32:39 +0100

    yes

--- formation-docker-sndio/script-setup.sh
+++ formation-docker-sndio/script-setup.sh
@@ -14,9 +14,9 @@ export PATH=${M2_HOME}/bin:${PATH} \
 chmod +x /etc/profile.d/maven.sh
 source /etc/profile.d/maven.sh
 # install psql
-sudo apt-get install curl ca-certificates gnupg -y
+apt-get install curl ca-certificates gnupg -y
 curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-sudo apt-get update
-sudo apt install postgresql-client-11
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+apt-get update
+apt install postgresql-client-11
 
